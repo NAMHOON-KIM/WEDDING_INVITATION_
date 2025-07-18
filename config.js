8970ccdda290717d2d8800513eb815a257bdefd3
NAMHOON-KIM <shtodn@gmail.com>
2025-07-19 04:40:42 +0900

    Update config.js

--- config.js
+++ config.js
@@ -3,17 +3,17 @@ export const KAKAOTALK_API_TOKEN = "JavaScript 키 입력";
 export const KAKAOTALK_SHARE_IMAGE =
   "https://cdn.pixabay.com/photo/2014/11/13/17/04/heart-529607_960_720.jpg";
 
-export const WEDDING_DATE = "1970년 01월 01일, 목요일 오전 12시 00분";
-export const WEDDING_LOCATION = "○○○웨딩, ○층 ○○홀";
+export const WEDDING_DATE = "2025년 10월 25일, 일요일 오전 11시 00분";
+export const WEDDING_LOCATION = "W스퀘어웨딩, 3층 더가든홀";
 
-export const GROOM_NAME = "○○○";
-export const GROOM_ACCOUNT_NUMBER = "○○은행 ***-***-******";
+export const GROOM_NAME = "김남훈";
+export const GROOM_ACCOUNT_NUMBER = "하나은행 323-910293-07107";
 export const GROOM_FATHER_NAME = "○○○";
 export const GROOM_FATHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
 export const GROOM_MOTHER_NAME = "○○○";
 export const GROOM_MOTHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
 
-export const BRIDE_NAME = "○○○";
+export const BRIDE_NAME = "김보연";
 export const BRIDE_ACCOUNT_NUMBER = "○○은행 ***-***-******";
 export const BRIDE_FATHER_NAME = "○○○";
 export const BRIDE_FATHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
